c75b07022908692712b2a7a8db2d589d700c8a16
marko <markodubravac123@gmail.com>
2024-04-25 17:38:26 +0200

    Update and details page

--- projects/routes/projects.js
+++ projects/routes/projects.js
@@ -15,8 +15,12 @@ router.post("/", async (req, res) => {
       start_date,
       end_date,
     });
-    await project.save();
-    res.redirect("/");
+    if (name === null || description === null || price === null) {
+      res.redirect("/projects/new?message=fill_all_Fields");
+    } else {
+      await project.save();
+      res.redirect("/");
+    }
   } catch (error) {
     res
       .status(400)
@@ -37,6 +41,62 @@ router.post("/:id", async (req, res) => {
     res
       .status(500)
       .json({ message: "Failed to delete project", error: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const project = await Project.findById(id);
+    if (!project) {
+      res.status(404).send("Project not found");
+    } else {
+      res.render("project-details", { project });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to fetch project details",
+      error: error.message,
+    });
+  }
+});
+
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const project = await Project.findById(id);
+    if (!project) {
+      res.status(404).send("Project not found");
+    } else {
+      res.render("edit-project-form", { project });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to fetch project details for editing",
+      error: error.message,
+    });
+  }
+});
+
+router.post("/:id/edit", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description, price, finished_jobs, start_date, end_date } =
+      req.body;
+    await Project.findByIdAndUpdate(id, {
+      name,
+      description,
+      price,
+      finished_jobs,
+      start_date,
+      end_date,
+    });
+    res.redirect(`/projects/${id}`);
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to update project details",
+      error: error.message,
+    });
   }
 });
 
